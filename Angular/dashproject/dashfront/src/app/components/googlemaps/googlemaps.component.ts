c90c20073b49339ccd19f79a49f21b8f7bcaf56e
Jibrugo <97877391+Jibrugo@users.noreply.github.com>
2023-03-21 17:57:24 +0100

    Update googlemaps.component.ts

--- Angular/dashproject/dashfront/src/app/components/googlemaps/googlemaps.component.ts
+++ Angular/dashproject/dashfront/src/app/components/googlemaps/googlemaps.component.ts
@@ -19,9 +19,35 @@ let mapOptions = {
 };
 
 function initMap(){
-  // Promise
+
 loader.load().then((google) => {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, mapOptions); // Creer la map
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, mapOptions); 
+
+  const centerButton = document.createElement("button");
+
+  centerButton.textContent = "Recentrer";
+  centerButton.classList.add("bouton-de-recentrage");
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerButton);
+  const latlng = { lat: 48.00746072080193, lng: 0.19740114421013952};
+  new google.maps.Marker({
+    position: latlng,
+    map,
+    title: "Le centre de la map",
+  });
+
+  centerButton.addEventListener("click", () => {
+          const pos = {
+            lat: 48.00746072080193,
+            lng: 0.19740114421013952
+          };
+          map.setCenter(pos);
+  }
+);
+
+
+
+
   });
   }
 
