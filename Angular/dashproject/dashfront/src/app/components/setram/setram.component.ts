d6effa600c60373a712a70ad1be14e5086549e56
Alexis Guimbert <alexisguimbert16@gmail.com>
2023-03-26 01:47:07 +0100

    Pos fict & réelle si besoin

--- Angular/dashproject/dashfront/src/app/components/setram/setram.component.ts
+++ Angular/dashproject/dashfront/src/app/components/setram/setram.component.ts
@@ -3,7 +3,7 @@ import { loader } from '../googlemaps/googlemaps.component';
 
 // distance à laquelle on affiche les lignes (en mètres)
 const distanceAffichageLigne = 100;
-
+export const utiliserPositionFictive = true;
 @Component({
   selector: 'app-setram',
   templateUrl: './setram.component.html',
@@ -18,21 +18,34 @@ export class SetramComponent implements OnInit{;
   }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition(
+       (position) => {
+        var positionDeLaPersonne = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
       fetch("http://localhost:4000/")
         .then(res => res.json())
         .then(data => {
         loader.load().then((google) => {
           var bdd = JSON.parse(JSON.stringify(data));
           bdd.forEach((element:any) => {
-            const positionDeLaPersonne = new google.maps.LatLng(48.00746072080193,0.19740114421013952);
-            const destination = new google.maps.LatLng(element[3],element[4]);
-            const distanceCalc = google.maps.geometry.spherical.computeDistanceBetween(positionDeLaPersonne, destination);
+            var positionFictive = new google.maps.LatLng(48.00746072080193,0.19740114421013952);
+            var destination = new google.maps.LatLng(element[3],element[4]);
+            if(utiliserPositionFictive)
+              var distanceCalc = google.maps.geometry.spherical.computeDistanceBetween(positionFictive, destination);
+            else
+              var distanceCalc = google.maps.geometry.spherical.computeDistanceBetween(positionDeLaPersonne, destination);
             if(distanceCalc < distanceAffichageLigne)
               this.infos.push({numero_ligne: element[0], nom_arret: element[1], lien: element[2], distance: Math.floor( distanceCalc )});
           });
           console.log(this.infos);
         });
       });
+
+    },
+    function errorCallback(error) {
+       console.log(error)
+    }
+ );
   }
 
 }
